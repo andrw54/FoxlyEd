7d92bc2767c4180507e25ae3152c04e4b2b4e4b0
Galvis <galvisdahi22@gmail.com>
2026-05-26 16:52:54 -0500

    Cambio en el inicio de sesión y bloqueo de pagina

--- JS/formulario.js
+++ JS/formulario.js
@@ -1,8 +1,34 @@
 console.log('Hola');
-const user = {
-    "email": "admin@admin.com",
-    "password": "admin123",
-    "usuario": "admin"
+const user = 
+[
+    {
+     "email": "admin@admin.com",
+     "password": "admin123",
+     "usuario": "Admin"
+    },
+    {
+     "email": "estudiante@estu.com",
+     "password": "estudiante123",
+     "usuario": "Estudiante"
+    },
+    {
+     "email": "maestro@maestro.com",
+     "password": "maestro123",
+     "usuario": "Maestro"
+    },
+
+]
+
+function inicioSesion(email, password)
+{
+    for (let index = 0; index < user.length; index++)
+    {
+        const element = user[index];
+        console.log("index", index);
+        console.log("element", element.)
+        
+    }
+  
 }
 
 const formulario = document.getElementById("formulario");
@@ -11,6 +37,7 @@ formulario.addEventListener("submit", function(event) {
     event.preventDefault();
     let email = document.getElementById("exampleInputEmail1").value;
     let password =document.getElementById("exampleInputPassword1").value;
+    inicioSesion (email, password);
     if (user.email === email && user.password === password)
     {
         sessionStorage.setItem("usuario", user.usuario);
